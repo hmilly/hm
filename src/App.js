e0b9156a1b2fda4716e08a10727e942fe8663102
hmilly <milstowers@gmail.com>
2023-05-06 15:36:32 +0100

    adding base files, base scss & app.js layout

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import "./app.scss";
+import Nav from "./components/Nav";
+import Profile from "./components/Profile";
+import About from "./components/About";
+import Stack from "./components/Stack";
+import Projects from "./components/Projects";
+import Footer from "./components/Footer";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Nav />
+      <div className="App-children">
+        <Profile />
+        <About />
+        <Stack />
+        <Projects />
+      </div>
+      <Footer />
     </div>
   );
 }
-
 export default App;
