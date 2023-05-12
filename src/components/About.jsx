5babf2d72a1431d9a3c56e44eafc910522b3e5a6
hmilly <milstowers@gmail.com>
2023-05-12 17:23:04 +0100

    fixed padding and bg issues

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,27 +3,29 @@ import "../styles/about.scss";
 const About = () => {
   return (
     <section id="about-container">
-      <div>
-        <h2>About</h2>
-        <p>
-          I'm an enthusiastic and skilled <span>web developer</span> who also
-          possesses a wealth of transferable skills and a passion for digital
-          innovation.
-        </p>
-        <p>
-          I have been interested in web design since myspace was around, but
-          have only taken to studying web development seriously in the last few
-          years.
-        </p>
-        <p>
-          I've mainly been working with <span>React</span>, but I'm open to so
-          much more! I’m currently seeking my first commercial role within
-          frontend development.
-        </p>
-        <p>
-          Please feel free to view my projects and github page, or if you know
-          of any job openings, please reach out on LinkedIn!
-        </p>
+      <div className="about-wrapper">
+        <div>
+          <h2>About</h2>
+          <p>
+            I'm an enthusiastic and skilled <span>web developer</span> who also
+            possesses a wealth of transferable skills and a passion for digital
+            innovation.
+          </p>
+          <p>
+            I have been interested in web design since myspace was around, but
+            have only taken to studying web development seriously in the last
+            few years.
+          </p>
+          <p>
+            I've mainly been working with <span>React</span>, but I'm open to so
+            much more! I’m currently seeking my first commercial role within
+            frontend development.
+          </p>
+          <p>
+            Please feel free to view my projects and github page, or if you know
+            of any job openings, please reach out on LinkedIn!
+          </p>
+        </div>
       </div>
     </section>
   );
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,13 +5,15 @@ import profile from "../images/profile.jpg";
 const Profile = () => {
   return (
     <section id="profile-container">
-      <aside>
-        <p className="lg-txt">Hi, my name is</p>
-        <h1>Holly Millard</h1>
-        <h2>Frontend developer</h2>
-        <Contact />
-      </aside>
-      <img src={profile} alt="page owner pic" />
+      <div>
+        <aside>
+          <p className="lg-txt">Hi, my name is</p>
+          <h1>Holly Millard</h1>
+          <h2>Frontend developer</h2>
+          <Contact />
+        </aside>
+        <img src={profile} alt="page owner pic" />
+      </div>
     </section>
   );
 };
